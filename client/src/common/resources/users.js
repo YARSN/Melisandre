113f79feb3c5bac7218da5b420aec66c8a5bd79d
Rob Kovtiuk <robsonheater@gmail.com>
2016-11-06 22:25:46 +0200

    Init entities

---
+++ client/src/common/resources/users.js
@@ -0,0 +1,10 @@
+ngular.module('resources.users', ['mongolabResource']);
+angular.module('resources.users').factory('Users', ['mongolabResource', function (mongoResource) {
+
+    var userResource = mongoResource('users');
+    userResource.prototype.getFullName = function () {
+        return this.lastName + " " + this.firstName + " (" + this.email + ")";
+    };
+
+    return userResource;
+}]);
